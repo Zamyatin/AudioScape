e8a32f71e0a15855f11d97f43ccaf49facafddcf
Raj Singh <raj@foreversingh.com>
2015-02-17 16:39:43 -0600

    adding abstraction between our song model and GrooveShark model

--- app/models/concerns/groove_shark_song.rb
+++ app/models/concerns/groove_shark_song.rb
@@ -3,14 +3,15 @@ module GrooveSharkSong
   
   attr_writer :grooveshark_client
   attr_writer :grooveshark_song
-  
+
   STOCK_IMG = '/images/devil_horns.jpg'
-  
+
   module InstanceMethods
+
     def grooveshark_client
       @grooveshark_client ||= Grooveshark::Client.new
     end
-  
+
     def grooveshark_song(link)
       @groovershark_song ||= grooveshark_client.get_song_by_id(link) # wrong method
     end
