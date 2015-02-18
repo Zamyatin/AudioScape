31e54c9a901ecf59759d80c416b1f0de2c53c347
Raj Singh <raj@foreversingh.com>
2015-02-17 23:13:59 -0600

    adding client query functionality to groove_session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
 
   def groove_session
-    client = Groveshark::Client.new(session: session[:groove_session])
-    return client
+    Grooveshark::Client.new(session: session[:groove_session])
   end
   
-
+  def from_whence_i_came
+    ref_path = URI(request.referer).path
+    return ref_path
+  end
+  
+  def grooveshark_url(link)
+    groove_session.get_song_url_by_id(link) #whichever
+  end
+  
 end
